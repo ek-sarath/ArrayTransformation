9a7293300694879c44262d7b6823d69a3971ae21
ek-sarath <sarath.e@edstem.com>
2024-10-29 11:42:08 +0530

    Functionality Implemented

--- Transformation/lib/index.js
+++ Transformation/lib/index.js
@@ -1,3 +1,24 @@
 "use strict";
-const a = 32;
-console.log(a);
+function groupBy(arr, key) {
+    return arr.reduce((acc, current) => {
+        const value = current[key];
+        if (value !== undefined || value !== null) {
+            if (!acc[value]) {
+                acc[value] = [];
+            }
+            acc[value].push(current);
+        }
+        return acc;
+    }, {});
+}
+const products = [
+    { id: 1, category: "electronics", subCategory: "adapter", price: 100 },
+    { id: 2, category: "books", subCategory: "fiction", price: 20 },
+    { id: 3, category: "electronics", subCategory: "mobile", price: 200 },
+    { id: 4, category: "electronics", subCategory: "adapter", price: 250 },
+    { id: 5, category: "books", subCategory: "fiction", price: 85 }
+];
+const grouped = groupBy(products, "category");
+console.log(grouped);
+const subCategory = groupBy(products, "subCategory");
+console.log(subCategory);
--- Transformation/src/index.ts
+++ Transformation/src/index.ts
@@ -1,2 +1,38 @@
-const a: number = 32
-console.log(a);
+    function groupBy<T extends Record<string, any>>(
+        arr: T[],
+        key: keyof T | string
+    ): Record<string, T[]>
+    
+    {
+        return arr.reduce((acc, current) => {
+            const value = current[key];
+            if (value !== undefined || value !== null) {
+                if (!acc[value]) {
+                    acc[value] = [];
+                }
+                acc[value].push(current);
+            }
+            return acc;
+        }, {} as Record<string, T[]>);
+    }
+    
+    interface product {    
+        id: number;
+        category: string;
+        subCategory?: string;
+        price: number;
+    }
+    
+    const products: product[] = [
+    { id: 1, category: "electronics", subCategory: "adapter", price: 100 },
+    { id: 2, category: "books", subCategory: "fiction", price: 20 },
+    { id: 3, category: "electronics", subCategory: "mobile", price: 200 },
+    { id: 4, category: "electronics", subCategory: "adapter", price: 250 },
+    { id: 5, category: "books", subCategory: "fiction", price: 85 }
+    ];
+    
+    const grouped = groupBy(products, "category");
+    console.log(grouped);
+
+    const subCategory = groupBy(products, "subCategory");
+    console.log(subCategory);
